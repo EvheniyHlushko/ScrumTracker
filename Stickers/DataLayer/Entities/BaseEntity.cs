3285fde4387cf479344e572e28c6124908ff04b7
Evgeniy Glushko <evgeniy.glushko@yahoo.com>
2016-05-27 22:39:03 +0300

    review

--- Stickers/DataLayer/Entities/BaseEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace DataLayer.Entities
-{
-    public abstract class BaseEntity
-    {
-        public Guid Id { get; set; }
-    }
-}
--- Stickers/StickersWeb/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StickersWeb.MvcApplication" Language="C#" %>
